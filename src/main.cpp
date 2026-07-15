e5280aca595aac6dbd5b4994228c12161e213a2f
stylo-codes-stuff <mistah6909@gmail.com>
2026-07-15 02:40:28 -0500

    add player class and the ability for enemies to kill the player

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 //basic std includes
 #include <iostream>
+#include <memory>
 //necessary includes for butano
 #include "bn_vector.h"
 #include "bn_keypad.h"
@@ -18,14 +19,69 @@
 //debug variables to make sound mixing easier
 float theme_volume = 0.5;
 float footstep_volume = 0.3;
+//DONT FORGET THAT THE NEXT THING YOU SHOULD DO IS MAKE A CLASS FOR POWERUPS LIKE THE WAGON WHEEL
+//classes for both players and enemies
+class Player{
+    public:
+    bn::sprite_ptr player_sprite;
+    int width = 32;
+    int height = 32;
+    int direction = 0;
+    bool alive = true;
+    Player(bn::sprite_ptr player_sprite) : player_sprite(player_sprite)
+    {
+        
+    }
+    void move_check(){
+        
+        //player movement checks
+        if(bn::keypad::right_held())
+        {
+            player_sprite.set_x(player_sprite.x() + 1);   
+            direction = 0;
+        }
+        if(bn::keypad::left_held())
+        {
+            player_sprite.set_x(player_sprite.x() - 1);
+            direction = 1;
+        }
+        
+        if(bn::keypad::up_held())
+        {
+            player_sprite.set_y(player_sprite.y() - 1);
+            direction = 2;
+        }
+        if(bn::keypad::down_held())
+        {
+            player_sprite.set_y(player_sprite.y() + 1);   
+            direction = 3;
+        }
+        
+    }
+    bn::rect get_bounds() const {
+        
+        bn::fixed_point pos = player_sprite.position();
+        return bn::rect(pos.x().integer() - (width / 2), pos.y().integer() - (height / 2), width, height);
+    }
+    bn::fixed getX(){
+        return player_sprite.x();
+    }
+    bn::fixed getY(){
+        return player_sprite.y();
+    }
+    void kill(){
+        alive = false;
+        player_sprite.set_visible(false);
+    }
+};
 //classes for both player and enemy/boss projectiles
 class playerProjectile
 {
     public:
     int direction;
     bn::sprite_ptr projectile_sprite;
-    int width = 16;
-    int height = 16;
+    int width = 4;
+    int height = 4;
     //bullet constructor
     playerProjectile(int direction, bn::sprite_ptr projectile_sprite) : direction(direction), projectile_sprite(projectile_sprite)
     {
@@ -33,6 +89,9 @@ class playerProjectile
     }
     
     void move(){
+        switch(direction){
+            
+        }
         if(direction == 0)
         {
             projectile_sprite.set_x(projectile_sprite.x() + 1);
@@ -97,16 +156,15 @@ class enemyProjectile{
     }
     
 };
-
 //class for enemies
-class enemy{
+class Enemy{
     public:
     bn::sprite_ptr enemy_sprite;
     //we will implement enemy types later
     int type; // the only enemy type we will implement is an enemy that runs towards you
     int height;
     int width;
-    enemy(int type, bn::sprite_ptr enemy_sprite) : type(type), enemy_sprite(enemy_sprite)
+    Enemy(int type, bn::sprite_ptr enemy_sprite) : type(type), enemy_sprite(enemy_sprite)
     {
         //set the scale of the enemy sprite
         enemy_sprite.set_scale(.5);
@@ -129,7 +187,6 @@ class enemy{
         
     }
 };
-
 //check if two rects collide with eachother
 bool collides(bn::rect b1, bn::rect b2){
     return b1.x() < b2.x() + b2.width() &&
@@ -142,12 +199,15 @@ int main()
 {
     
     bn::core::init();
+    //lives counter 
+    int lives = 5;
     //bools to set which powerups are active
     //bool upgrade_active = false;
     //bool wagon_wheel = false;
     //bool coffee = false;
     //bool machine_gun = false;
     //bool badge = false;
+    bool gravestone = false;
     //int stored_upgrade = 0;
     //start theme music
     bn::music_items::theme.play(theme_volume);
@@ -156,15 +216,13 @@ int main()
     //create vector for enemy projectiles
     bn::vector<enemyProjectile, 10> enemy_projectiles;
     //create vector for enemies
-    bn::vector<enemy, 30> enemies;
-    //initalize sprite for player
-    bn::sprite_ptr player = bn::sprite_items::player.create_sprite(0, 0);
-    //initialize test enemy
-    bn::sprite_ptr test_enemy = bn::sprite_items::player.create_sprite(0,0);
-    enemies.push_back(enemy(0,test_enemy));
-    int player_direction = 0; //0 = right, 1 = left, 2 = up, 3 = down, 4
+    bn::vector<Enemy, 30> enemies;
+    //create
+    //initalize sprite for player and create actual object
+    bn::sprite_ptr player_sprite = bn::sprite_items::player.create_sprite(0, 0);
+    Player player(player_sprite);
+    //
     //tracker to keep track of how many frames are left uintil the player is able to shoot again
-    //the gameboy runs at 60 fps so the shooting cooldown should probably be around 30
     int player_shooting_cooldown = 0;
     //tracker for how often the footstep sound plays
     int footstep_cooldown = 0;
@@ -179,29 +237,8 @@ int main()
                 footstep_cooldown = 20;
             }
         }
-        //player movement checks
-        if(bn::keypad::right_held())
-        {
-            player.set_x(player.x() + 1);   
-            player_direction = 0;
-        }
-        if(bn::keypad::left_held())
-        {
-            player.set_x(player.x() - 1);
-            player_direction = 1;
-        }
-        
-        if(bn::keypad::up_held())
-        {
-            player.set_y(player.y() - 1);
-            player_direction = 2;
-        }
-        if(bn::keypad::down_held())
-        {
-            player.set_y(player.y() + 1);   
-            player_direction = 3;
-        }
-        
+        //move the player if any dpad buttons are held
+        player.move_check();
         //player fire checks
         if(bn::keypad::a_held())
         {
@@ -211,14 +248,25 @@ int main()
                 // check if vector is full before creating a new projectile to avoid overflow
                 if(!projectiles.full())
                 {
-                    bn::sprite_ptr projectile_sprite = bn::sprite_items::bullet.create_sprite(player.x(), player.y());
-                    projectiles.push_back(playerProjectile(player_direction, projectile_sprite));
+                    bn::sprite_ptr projectile_sprite = bn::sprite_items::bullet.create_sprite(player.getX(), player.getY());
+                    projectiles.push_back(playerProjectile(player.direction, projectile_sprite));
                 }
                 //set shoot cooldown to forty frames to stagger the amount of projectiles
                 player_shooting_cooldown = 30;
             }
         }
-        
+        //check if any enemies are colliding with the enemy
+        auto enemy = enemies.begin();
+        while(enemy != enemies.end()){
+            enemy->move();
+            bn::rect playerBB = player.get_bounds();
+            bn::rect enemyBB = enemy->get_bounds();
+            if(collides(playerBB,enemyBB)){
+                enemies.erase(enemy);
+                ene
+                player.kill();
+            }
+        }
         //check to see if any projectiles are off screen and remove them from the vector if they are
         //if theyre not move them
         auto projectile = projectiles.begin();
